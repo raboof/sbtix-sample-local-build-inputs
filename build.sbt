a93fd1271f2ea056218a6c0e4ca77868b595a1ce
Arnout Engelen <arnout@bzzt.net>
2023-08-31 09:43:25 +0200

    Example project, no custom branch yet

---
+++ build.sbt
@@ -0,0 +1,7 @@
+scalaVersion := "3.3.0"
+
+libraryDependencies ++= Seq(
+  // TODO depend on 3.1.1 from https://github.com/raboof/scala-scraper/tree/branch
+  // instead
+  "net.ruippeixotog" %% "scala-scraper" % "3.1.0",
+)
---
+++ src/main/scala/Main.scala
@@ -0,0 +1,7 @@
+@main
+def main =
+  // TODO use custom API from branch
+  // TODO use class from additional transitive commons-vfs2 dependency
+  // TODO check manually that nscala-time is no longer part of the
+  // build closure
+  println("Hello, world")
